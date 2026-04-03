8850b2afa5c77e6315cb0ed825d8e2b6fe8a637e
sujithgoud2210 <sujith200322@gmail.com>
2026-04-03 23:39:04 +0530

    static

--- src/com/java/oops/staticexample/StaticInt.java
+++ src/com/java/oops/staticexample/StaticInt.java
@@ -3,5 +3,12 @@ package com.java.oops.staticexample;
 public class StaticInt {
     static int a=4;
     static int b;
+static {
+    System.out.println("im in static block");
+    b=a*5;
+}
 
+    public static void main(String[] args) {
+        StaticInt obj =new StaticInt();
+    }
 }
