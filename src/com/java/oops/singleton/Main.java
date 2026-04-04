dd4756251e287b5424046a642c053a8164d0a50b
sujithgoud2210 <sujith200322@gmail.com>
2026-04-05 00:07:45 +0530

    singleton

--- src/com/java/oops/singleton/Main.java
+++ src/com/java/oops/singleton/Main.java
@@ -2,7 +2,6 @@ package com.java.oops.singleton;
 
 public class Main {
     public static void main(String[] args) {
-        Singleton obj =new Singleton();
-        obj.num
+        Singleton obj= Singleton.getInstance();
     }
 }
--- src/com/java/oops/singleton/Singleton.java
+++ src/com/java/oops/singleton/Singleton.java
@@ -1,8 +1,17 @@
 package com.java.oops.singleton;
 
+import java.util.Stack;
+
 public class Singleton {
    private int num=0;
    private Singleton(){
 
     }
+    public static Singleton instance;
+   public static Singleton getInstance(){
+       if(instance==null){
+           instance=new Singleton();
+       }
+       return instance;
+   }
 }
